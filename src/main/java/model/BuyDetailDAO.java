83abde0ad044357dbe3146895ecebb2f288c15c9
jihyean <d9rmlatte@gmail.com>
2023-07-17 03:24:48 +0900

    BuyDetail VO / DAO 작성

--- src/main/java/model/BuyDetailDAO.java
+++ src/main/java/model/BuyDetailDAO.java
@@ -3,16 +3,109 @@ package model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class BuyDetailDAO {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	
-	static final private String SQL_SELECTALL = "";
-	static final private String SQL_SELECTONE = "";
-	static final private String SQL_INSERT = "";
-	static final private String SQL_UPDATE = "";
-	static final private String SQL_DELETE = "";
+
+	static final private String SQL_SELECTALL = "SELECT bt.BUY_SERIAL, bt.BUY_NUM, t.TEA_NUM, bt.BUY_CNT, t.TEA_NAME\r\n"
+			+ "FROM BUY_DETAIL bt JOIN TEA t USING (TEA_NUM) WHERE bt.BUY_NUM= ?;";
+	static final private String SQL_SELECTONE = "SELECT BUY_SERIAL, BUY_NUM, TEA_NUM, BUY_CNT\r\n"
+			+ "FROM BUY_DETAIL WHERE BUY_SERIAL= ?;";
+	static final private String SQL_INSERT = "INSERT INTO BUY_DETAIL(BUY_NUM, TEA_NUM, BUY_CNT)\r\n"
+			+ "VALUES(?, ?, ?);";
+
+	public ArrayList<BuyDetailVO> selectAll(BuyDetailVO btVO) {
+
+		conn = JDBCUtil.connect();
+
+		ArrayList<BuyDetailVO> datas = new ArrayList<BuyDetailVO>();
+
+		try {
+			pstmt = conn.prepareStatement(SQL_SELECTALL);
+			pstmt.setInt(1, btVO.getBuyNum());
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				BuyDetailVO data = new BuyDetailVO();
+				data.setBuySerial(rs.getInt("BUY_SERIAL"));
+				data.setBuyNum(rs.getInt("BUY_NUM"));
+				data.setTeaNum(rs.getInt("TEA_NUM"));
+				data.setBuyCnt(rs.getInt("BUY_CNT"));
+				data.setTeaName(rs.getString("TEA_NAME"));
+				datas.add(data);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		JDBCUtil.disconnect(rs, pstmt, conn);
+
+		return datas;
+
+	}
+
+	public BuyDetailVO selectOne(BuyDetailVO btVO) {
+		conn = JDBCUtil.connect();
+		BuyDetailVO data = null;
+
+		try {
+
+			pstmt = conn.prepareStatement(SQL_SELECTONE);
+			pstmt.setInt(1, btVO.getBuySerial());
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				data = new BuyDetailVO();
+				data.setBuySerial(rs.getInt("BUY_SERIAL"));
+				data.setBuyNum(rs.getInt("BUY_NUM"));
+				data.setTeaNum(rs.getInt("TEA_NUM"));
+				data.setBuyCnt(rs.getInt("BUY_CNT"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		JDBCUtil.disconnect(rs, pstmt, conn);
+
+		return data;
+
+	}
+
+	public boolean insert(BuyDetailVO btVO) {
+		conn = JDBCUtil.connect();
+
+		try {
+			pstmt = conn.prepareStatement(SQL_INSERT);
+			pstmt.setInt(1, btVO.getBuyNum());
+			pstmt.setInt(2, btVO.getTeaNum());
+			pstmt.setInt(3, btVO.getBuyCnt());
+
+			int rs = pstmt.executeUpdate();
+			if (rs <= 0) {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		JDBCUtil.disconnect(pstmt, conn);
+
+		return true;
+	}
+
+	public boolean update(BuyDetailVO btVO) {
+		return false;
+	}
+
+	public boolean delete(BuyDetailVO btVO) {
+		return false;
+	}
 
 }
--- src/main/java/model/BuyDetailVO.java
+++ src/main/java/model/BuyDetailVO.java
@@ -1,5 +1,65 @@
 package model;
 
 public class BuyDetailVO {
+	private int buySerial;
+	private int buyNum;
+	private int teaNum;
+	private int buyCnt;
+	
+	// 임시변수
+	private String teaName;
+	
+	public BuyDetailVO() {
+		this(0, 0, 0, 0);
+	}
+	
+	public BuyDetailVO(int buySerial, int buyNum, int teaNum, int buyCnt) {
+		this.buySerial = buySerial;
+		this.buyNum = buyNum;
+		this.teaNum = teaNum;
+		this.buyCnt = buyCnt;
+	}
 
+	public int getBuySerial() {
+		return buySerial;
+	}
+
+	public void setBuySerial(int buySerial) {
+		this.buySerial = buySerial;
+	}
+
+	public int getBuyNum() {
+		return buyNum;
+	}
+
+	public void setBuyNum(int buyNum) {
+		this.buyNum = buyNum;
+	}
+
+	public int getTeaNum() {
+		return teaNum;
+	}
+
+	public void setTeaNum(int teaNum) {
+		this.teaNum = teaNum;
+	}
+
+	public int getBuyCnt() {
+		return buyCnt;
+	}
+
+	public void setBuyCnt(int buyCnt) {
+		this.buyCnt = buyCnt;
+	}
+
+	public String getTeaName() {
+		return teaName;
+	}
+
+	public void setTeaName(String teaName) {
+		this.teaName = teaName;
+	}
+	
+	
+	
 }
