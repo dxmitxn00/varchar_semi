4da60e6089ecd0df7c60091bc4381b04092bd7c7
dxmitxn00 <dxmitxn00@naver.com>
2023-07-19 16:17:50 +0900

    orderDetail 주석 수정, Footer태그 수정

--- src/main/java/controller/SignupAction.java
+++ src/main/java/controller/SignupAction.java
@@ -6,16 +6,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.MemberDAO;
+import model.MemberVO;
+
 public class SignupAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		ActionForward forward = new ActionForward();
+		ActionForward forward = null;
 		
-//		forward.setRedirect(true);
-//		forward.setPath("");
+		MemberDAO mDAO = new MemberDAO();
+		MemberVO mVO = new MemberVO();
+		
+		mVO.setMemberId(request.getParameter("memberId"));
+		mVO.setMemberPw(request.getParameter("memberPw"));
+		mVO.setMemberName(request.getParameter("memberName"));
+		mVO.setMemberAddress(request.getParameter("memberAddress"));
+		mVO.setMemberPhone(Long.parseLong(request.getParameter("memberPhone")));
+		mVO.setMemberEmail(request.getParameter("memberEmail"));
+		
+		if (mDAO.insert(mVO)) {
+			forward = new ActionForward();
+			forward.setRedirect(true);
+			forward.setPath("main.do");
+		}
 		
 		return forward;
 	}
